14147daa06b10ed27c838b259ac3c3fb556f16e1
wchhuangya <wchhuangyawork@163.com>
2015-06-30 08:37:03 +0800

    无奈的提交
    不知道干了什么~Githu for windows打开后~看不到本地的修改记录~没有办法~项目删除了重新来了一次~

---
+++ sdf/src/com/ch/wchhuangya/dzah/entity/sys/SysUser.java
@@ -0,0 +1,70 @@
+package com.ch.wchhuangya.dzah.entity.sys;
+// default package
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+
+/**
+ * SysUser entity. @author MyEclipse Persistence Tools
+ */
+@Entity
+@Table(name="sys_user")
+public class SysUser  implements java.io.Serializable {
+
+
+    // Fields    
+
+     private String userId;
+     private String userName;
+
+
+    // Constructors
+
+    /** default constructor */
+    public SysUser() {
+    }
+
+    
+    /** full constructor */
+    public SysUser(String userName) {
+        this.userName = userName;
+    }
+
+   
+    // Property accessors
+    @GenericGenerator(name="generator", strategy="uuid.hex")@Id @GeneratedValue(generator="generator")
+    
+    @Column(name="user_id", unique=true, nullable=false, length=32)
+
+    public String getUserId() {
+        return this.userId;
+    }
+    
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    
+    @Column(name="user_name", nullable=false, length=64)
+
+    public String getUserName() {
+        return this.userName;
+    }
+    
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+   
+
+
+
+
+
+
+
+
+}
